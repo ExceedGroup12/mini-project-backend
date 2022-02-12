61e2177aed49db0028c893c5007f829599a6c64f
Nabhan Suwanachote <nabhan.au@gmail.com>
2022-02-12 13:36:57 +0700

    finish get data of toilet

--- status.py
+++ status.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-import time
+from datetime import datetime
 
 client = MongoClient('mongodb://localhost', 27017)
 
@@ -8,18 +8,29 @@ menu_collection = db["status"]
 
 def get_data(room:str):
     r = menu_collection.find({"room":room})
-    diff = compute_diff_time(r["first"], r["last"])
+    r = list(r)
+    if len(r) == 0:
+        return {
+        "room":None,
+        "status":"Not Found",
+        "diff": None
+        }
+    diff = compute_diff_time(r[0]["first"], r[0]["last"])
+    dt_object = datetime.fromtimestamp(r[0]["first"])
     return{
         "room":room,
-        "status":r["status"],
-        "diff": diff
+        "status":r[0]["status"],
+        "diff": diff,
+        "first": dt_object
     }
     
 def compute_diff_time(first, last):
     if first == None:
         diff = None
     elif last == None:
-        diff = time.time() - first
+        now = datetime.now()
+        timestamp = datetime.timestamp(now)
+        diff = timestamp - first
     else:
         diff = last - first
     return diff
@@ -28,16 +39,11 @@ def get_all_data():
     data = []
     r = menu_collection.find()
     for rooms in r:
-        print(rooms)
+        dt_object = datetime.fromtimestamp(rooms["first"])
         data.append({
             "room":rooms["room"],
             "status":rooms["status"],
-            "diff": compute_diff_time(rooms["first"], rooms["last"])
+            "diff": compute_diff_time(rooms["first"], rooms["last"]),
+            "first": dt_object
         })
     return data
-
-def compute_estimate_time():
-    r = get_all_data()
-    
-    
-print(get_all_data())
--- url.py
+++ url.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from status import get_all_data, get_data
 
-from status import get_status
 app = FastAPI()
 
 @app.get("/status/{room}")
 def get_status(room:str):
-    return get_status(room)
+    return get_data(room)
 
-@app.get("/status/all}")
+@app.get("/allStatus")
 def get_all_status():
-    return get_all_status()
+    return get_all_data()
